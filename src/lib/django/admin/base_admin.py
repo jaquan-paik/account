d7534fa9fddf08332d30aec1c3b95d6659fe9329
Junheong Park <jh.park@ridi.com>
2018-02-20 18:41:04 +0900

    django admin action 에 delete_selected가 있는 경우만 삭제 하도록 함

--- src/lib/django/admin/base_admin.py
+++ src/lib/django/admin/base_admin.py
@@ -7,7 +7,8 @@ class BaseModelAdmin(admin.ModelAdmin):
     def get_actions(self, request):
         # Disable delete
         actions = super().get_actions(request)
-        del actions['delete_selected']
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
         return actions
 
     def has_add_permission(self, request) -> bool:
