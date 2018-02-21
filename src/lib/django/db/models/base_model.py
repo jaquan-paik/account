49b7b464619922747df9738f864787ba50365367
Junheong Park <jh.park@ridi.com>
2018-02-21 12:41:57 +0900

    IDE 자동완성을 위해 BaseModel 에 Manager를 명시함

--- src/lib/django/db/models/base_model.py
+++ src/lib/django/db/models/base_model.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
+from django.db.models import Manager
 
 
 class BaseModel(models.Model):
     created = models.DateTimeField(auto_now_add=True, editable=False, verbose_name='등록일')
     last_modified = models.DateTimeField(auto_now=True, editable=False, verbose_name='수정일')
 
+    objects = Manager()
+    
     class Meta:
         abstract = True
 
