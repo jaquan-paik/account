e80205b455784adcfacc43342732b35ff76291b2
Junheong Park <jh.park@ridi.com>
2018-02-23 15:39:51 +0900

    요청자의 ip 가 ridi ip 인지 확인 하는 로직을 util 로 옮김

--- src/lib/ridibooks/middlewares.py
+++ src/lib/ridibooks/middlewares.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AnonymousUser
 from django.http import HttpResponseForbidden
 from django.utils.deprecation import MiddlewareMixin
 
+from lib.ridibooks.utils import is_ridi_ip_from_request
 from .api.api_helper import RidiApiHelper
 from .exceptions import RidibooksException
 from lib.utils.ip import get_client_ip_from_request, is_internal_ip
@@ -13,9 +14,7 @@ UserModel = get_user_model()
 
 class RidiIPFilterMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        client_ip = get_client_ip_from_request(request)
-
-        if not is_internal_ip(client_ip) and not RidiIP.is_ridi_ip(client_ip):
+        if not is_ridi_ip_from_request(request):
             return HttpResponseForbidden()
         return None
 
---
+++ src/lib/ridibooks/utils.py
@@ -0,0 +1,7 @@
+from lib.ridibooks.ridi_ip import RidiIP
+from lib.utils.ip import get_client_ip_from_request, is_internal_ip
+
+
+def is_ridi_ip_from_request(request) -> bool:
+    client_ip = get_client_ip_from_request(request)
+    return is_internal_ip(client_ip) or RidiIP.is_ridi_ip(client_ip)
