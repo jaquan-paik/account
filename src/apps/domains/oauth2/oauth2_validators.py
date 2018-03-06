cbf48a1105df54b37c0dfe3e4648dddffdf874f4
Junheong Park <jh.park@ridi.com>
2018-03-06 14:55:10 +0900

    RidiOAuth2Validator 추가 - Access 토큰을 저장하지 않고, Refresh 토큰을 사용하고 revoke 하지 않도록 함

--- src/apps/domains/oauth2/oauth2_validators.py
+++ src/apps/domains/oauth2/oauth2_validators.py
@@ -1,18 +1,49 @@
-from __future__ import unicode_literals
+from datetime import datetime, timedelta
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import transaction
 from oauth2_provider.exceptions import FatalClientError
-from oauth2_provider.oauth2_validators import AccessToken, OAuth2Validator, RefreshToken
+from oauth2_provider.oauth2_validators import OAuth2Validator, RefreshToken
 from oauth2_provider.settings import oauth2_settings
 
+from apps.domains.oauth2.exceptions import JwtTokenErrorException
+from apps.domains.oauth2.token import JwtHandler
 from lib.log.logger import logger
 
 
 class RidiOAuth2Validator(OAuth2Validator):
-    def validate_bearer_token(self, token, scopes, request):
-        logger.info('-----------------------------------------------------------------------  ㅍ미ㅑㅇㅁㅅㄷ_ㅍㄷㅁㄱㄷㄱ ')
+    def get_id_token(self, token, token_handler, request):
         raise NotImplementedError()
+
+    def validate_silent_authorization(self, request):
+        raise NotImplementedError()
+
+    def validate_silent_login(self, request):
+        raise NotImplementedError()
+
+    def validate_user_match(self, id_token_hint, scopes, claims, request):
+        raise NotImplementedError()
+
+    def validate_bearer_token(self, token, scopes, request):
+        if not token:
+            return False
+
+        try:
+            access_token = JwtHandler.get_access_token(token)
+        except JwtTokenErrorException:
+            logger.error('Jwt token error. token: %s', token)
+            return False
+
+        if not access_token.is_valid(scopes):
+            return False
+
+        request.client = access_token.application
+        request.user = access_token.user
+        request.scopes = scopes
+
+        # this is needed by django rest framework
+        request.access_token = access_token
+        return True
 
     @transaction.atomic
     def save_bearer_token(self, token, request, *args, **kwargs):
@@ -22,48 +53,35 @@ class RidiOAuth2Validator(OAuth2Validator):
         if request.grant_type == 'client_credentials':
             request.user = None
 
+        # TODO: check out a more reliable way to communicate expire time to oauthlib
+        token['expires_in'] = oauth2_settings.ACCESS_TOKEN_EXPIRE_SECONDS
+
         refresh_token_code = token.get('refresh_token', None)
         if not refresh_token_code:
             return
 
-        refresh_token_instance = getattr(request, "refresh_token_instance", None)
-        if isinstance(refresh_token_instance, RefreshToken):
-            logger.info('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-
-        if not self.rotate_refresh_token(request) and isinstance(refresh_token_instance, RefreshToken):
-            logger.info('-----------------------------------------------------------------------------')
-            pass
-        else:
-            if isinstance(refresh_token_instance, RefreshToken):
-                try:
-                    refresh_token_instance.revoke()
-                except (AccessToken.DoesNotExist, RefreshToken.DoesNotExist):
-                    pass
-                else:
-                    setattr(request, "refresh_token_instance", None)
-
+        if self.rotate_refresh_token(request):
             refresh_token = RefreshToken(
                 user=request.user,
                 token=refresh_token_code,
                 application=request.client,
+                expires=datetime.now() + timedelta(seconds=oauth2_settings.REFRESH_TOKEN_EXPIRE_SECONDS),
+                scope=token['scope'],
             )
             refresh_token.save()
 
-        # TODO: check out a more reliable way to communicate expire time to oauthlib
-        token['expires_in'] = oauth2_settings.ACCESS_TOKEN_EXPIRE_SECONDS
+        token['refresh_token_expires_in'] = oauth2_settings.REFRESH_TOKEN_EXPIRE_SECONDS
 
     def revoke_token(self, token, token_type_hint, request, *args, **kwargs):
+        # Refresh 토큰만 Revoke 가능하다.
         if token_type_hint not in ['refresh_token']:
-            token_type_hint = None
+            return
 
-        token_types = {
-            'refresh_token': RefreshToken,
-        }
-
-        token_type = token_types.get(token_type_hint, AccessToken)
         try:
-            token_type.objects.get(token=token).revoke()
+            RefreshToken.objects.get(token=token).revoke()
         except ObjectDoesNotExist:
-            for other_type in [_t for _t in token_types.values() if _t != token_type]:
-                # slightly inefficient on Python2, but the queryset contains only one instance
-                list(map(lambda t: t.revoke(), other_type.objects.filter(token=token)))
+            pass
+
+    def get_original_scopes(self, refresh_token, request, *args, **kwargs):
+        rt = request.refresh_token_instance
+        return rt.scope
