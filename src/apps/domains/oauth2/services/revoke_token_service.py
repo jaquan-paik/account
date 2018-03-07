a89bc93482adc7fb3e17f04955ed6ae75607f78c
Junheong Park <jh.park@ridi.com>
2018-03-07 19:48:43 +0900

    DB부하를 줄이기 위해 revoke 토큰을 한 번에 다 하는게 아니라 1000개씩 쪼개서 한다. 1 iteration 후 1초 쉰다

--- src/apps/domains/oauth2/services/revoke_token_service.py
+++ src/apps/domains/oauth2/services/revoke_token_service.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from time import sleep
+from typing import List
 
 from django.db import transaction
 from oauth2_provider.oauth2_validators import Grant, RefreshToken
@@ -10,8 +12,32 @@ class RevokeTokenService:
         now = datetime.now()
         cls.revoke_by_expires(now)
 
+    @classmethod
+    def revoke_by_expires(cls, expires: datetime, revoke_per_iter: int=1000):
+        while True:
+            refresh_tokens = RefreshToken.objects.filter(expires__lte=expires)[0:revoke_per_iter]
+            if len(refresh_tokens) == 0:
+                break
+
+            cls.revoke_refresh_tokens(refresh_tokens)
+            sleep(1)
+
+        while True:
+            grants = Grant.objects.filter(expires__lte=expires)[0:revoke_per_iter]
+            if len(grants) == 0:
+                break
+
+            cls.revoke_grants(grants)
+            sleep(1)
+
     @staticmethod
     @transaction.atomic()
-    def revoke_by_expires(expires: datetime):
-        RefreshToken.objects.filter(expires__lte=expires).delete()
-        Grant.objects.filter(expires__lte=expires).delete()
+    def revoke_refresh_tokens(refresh_tokens: List[RefreshToken]):
+        for refresh_token in refresh_tokens:
+            refresh_token.revoke()
+
+    @staticmethod
+    @transaction.atomic()
+    def revoke_grants(grants: List[Grant]):
+        for grant in grants:
+            grant.revoke()
