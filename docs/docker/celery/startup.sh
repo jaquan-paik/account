9d960b5e681db0ef0b47ffe41ebe7175529cf2f1
김정근 <snorlax@ridi.com>
2018-03-09 19:26:03 +0900

    celery start 파일 수정

--- docs/docker/celery/startup.sh
+++ docs/docker/celery/startup.sh
@@ -7,18 +7,20 @@ trap gracefulShutdown SIGTERM
 
 if [ "$ENABLE_BEAT" == "1" ]
 then
-    BEAT_OPT="-B "
+    /usr/local/bin/celery beat \
+        -A $CELERY_APP_NAME \
+        -b $CELERY_BROKER_URL \
+        --workdir=/htdocs/www/src \
+        --pidfile=/var/run/celery \
+        --loglevel=INFO
 else
-    BEAT_OPT=""
+    /usr/local/bin/celery worker \
+        -A $CELERY_APP_NAME \
+        $BEAT_OPT \
+        -Q $CELERY_QUEUE \
+        -b $CELERY_BROKER_URL \
+        -c $CELERY_WORKER_COUNT \
+        --workdir=/htdocs/www/src \
+        --pidfile=/var/run/celery \
+        --loglevel=INFO
 fi
-
-/usr/local/bin/celery worker \
-    -A $CELERY_APP_NAME \
-    $BEAT_OPT \
-    -Q $CELERY_QUEUE \
-    -b $CELERY_BROKER_URL \
-    -c $CELERY_WORKER_COUNT \
-    --workdir=/htdocs/www/src \
-    --logfile=/htdocs/www/logs/celery.log \
-    --pidfile=/var/run/celery \
-    --loglevel=INFO
