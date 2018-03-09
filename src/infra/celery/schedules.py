6d1647cced1e1a2673e86cafbff1376cc3924377
Junheong Park <jh.park@ridi.com>
2018-03-09 16:26:34 +0900

    잘못된 schedule 함수 호출 수정

--- src/infra/celery/schedules.py
+++ src/infra/celery/schedules.py
@@ -6,7 +6,7 @@ from infra.celery.constants import CeleryQueue
 def get_celery_beat_schedule():
     return {
         'revoke-expired-tokens': {
-            'task': 'apps.domains.example.tasks.debug_task1',
+            'task': 'apps.domains.oauth2.tasks.revoke_expired_tokens',
             'schedule': crontab(hour='4'),
             'args': (),
             'options': {
