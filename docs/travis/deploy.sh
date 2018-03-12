c0384b816a2c6bde70964adfac128eab1f7ce029
김정근 <snorlax@ridi.com>
2018-03-12 17:46:13 +0900

    travis deploy test

---
+++ docs/travis/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+echo $TRAVIS_COMMIT
+echo $TRAVIS_PULL_REQUEST_SHA
+
+# generate settings
+#make ci-settings ns=$ENV access_key=$AWS_ACCESS_KEY_ID secret_key=$AWS_SECRET_ACCESS_KEY region=$AWS_DEFAULT_REGION
+#
+## aws ecr login
+#"`aws ecr get-login --no-include-email --region=$AWS_DEFAULT_REGION`"
+#
+## build image
+#make ci-build-account env=$ENV
+#make ci-build-celery env=$ENV
+#
+## tag image
+#make ci-tag-library env=$ENV ecr_path=$LIBRARY_ECR tag=latest
+#make ci-tag-library env=$ENV ecr_path=$LIBRARY_ECR tag=$CI_COMMIT_SHA
+#make ci-tag-celery env=$ENV ecr_path=$LIBRARY_ECR tag=latest
+#make ci-tag-celery env=$ENV ecr_path=$LIBRARY_ECR tag=$CI_COMMIT_SHA
+#
+## push image
+#- make ci-push-library env=$ENV ecr_path=$LIBRARY_ECR tag=latest
+#- make ci-push-library env=$ENV ecr_path=$LIBRARY_ECR tag=$CI_COMMIT_SHA
+#- make ci-push-celery env=$ENV ecr_path=$LIBRARY_ECR tag=latest
+#- make ci-push-celery env=$ENV ecr_path=$LIBRARY_ECR tag=$CI_COMMIT_SHA
+
+# deploy
+#- ecs deploy --tag=$CI_COMMIT_SHA --region=$AWS_DEFAULT_REGION --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY library-cluster library-service-all
+#- ecs deploy --tag=$CI_COMMIT_SHA --region=$AWS_DEFAULT_REGION --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY library-cluster library-service-api
+#- ecs deploy --tag=$CI_COMMIT_SHA --region=$AWS_DEFAULT_REGION --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY library-cluster library-service-www
