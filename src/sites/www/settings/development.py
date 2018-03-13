d1a690a880f3d14e67efc3a073338df8c8be5f49
김정근 <snorlax@ridi.com>
2018-03-13 21:24:16 +0900

    로그셋팅 제거

--- src/sites/www/settings/development.py
+++ src/sites/www/settings/development.py
@@ -7,7 +7,7 @@ from .base import *  # flake8: noqa: F403  # pylint:disable=wildcard-import
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-setup_logging(SITE, LogLevel.DEBUG, LOG_DIR)
+# setup_logging(SITE, LogLevel.DEBUG, LOG_DIR)
 
 SITE_DOMAIN = 'account.ridi.io'
 ROOT_DOMAIN = 'ridi.io'
