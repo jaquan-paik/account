afbff44c4103dec39907ec5767bc4a73449b28b0
김정근 <snorlax@ridi.com>
2018-03-13 17:58:33 +0900

    deploy 코드 수정

--- docs/travis/deploy.sh
+++ docs/travis/deploy.sh
@@ -31,7 +31,5 @@ make ci-push-account env=$ENV ecr_path=$ACCOUNT_ECR tag=$COMMIT_SHA
 make ci-push-celery env=$ENV ecr_path=$ACCOUNT_ECR tag=latest
 make ci-push-celery env=$ENV ecr_path=$ACCOUNT_ECR tag=$COMMIT_SHA
 
-## deploy
-#- ecs deploy --tag=$COMMIT_SHA --region=$AWS_DEFAULT_REGION --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY library-cluster library-service-all
-#- ecs deploy --tag=$COMMIT_SHA --region=$AWS_DEFAULT_REGION --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY library-cluster library-service-api
-#- ecs deploy --tag=$COMMIT_SHA --region=$AWS_DEFAULT_REGION --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY library-cluster library-service-www
+# deploy
+#ecs deploy --tag=$COMMIT_SHA --region=$AWS_DEFAULT_REGION --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY account-scalable-cluster account-scalable-service
