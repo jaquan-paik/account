9bbe23d970e69b82a25dafed4552988e912c2b2f
김정근 <snorlax@ridi.com>
2018-03-14 13:52:02 +0900

    deploy script수정

--- docs/travis/deploy.sh
+++ docs/travis/deploy.sh
@@ -36,4 +36,4 @@ ecs deploy --tag=$COMMIT_SHA --region=$AWS_DEFAULT_REGION \
     --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY \
     -i account-nginx $ACCOUNT_ECR/$ENV/account/nginx-www:latest \
     --timeout 600 \
-    account-scalable-cluster account-scalable-service
+    account-scalable-cluster account-scalable
