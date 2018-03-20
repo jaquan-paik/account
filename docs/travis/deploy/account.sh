04b715e9b279db97113a9684c25488f228192daf
김정근 <snorlax@ridi.com>
2018-03-20 16:43:59 +0900

    Remove whitespace

--- docs/travis/deploy/account.sh
+++ docs/travis/deploy/account.sh
@@ -8,7 +8,6 @@ ecs deploy --tag=$COMMIT_SHA --region=$AWS_DEFAULT_REGION \
     -i account-nginx $ACCOUNT_ECR/$ENVIRONMENT/account/nginx-www:latest \
     --timeout 600 \
     account-scalable-cluster account-scalable & \
-
 ecs deploy --tag=$COMMIT_SHA --region=$AWS_DEFAULT_REGION \
     --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY \
     -i account-nginx $ACCOUNT_ECR/$ENVIRONMENT/account/nginx-admin:latest \
--- docs/travis/deploy/celery.sh
+++ docs/travis/deploy/celery.sh
@@ -6,15 +6,12 @@ ecs deploy --tag=$COMMIT_SHA --region=$AWS_DEFAULT_REGION \
     --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY \
     --timeout 600 \
     account-fixed-cluster account-celery-beat & \
-
 ecs deploy --tag=$COMMIT_SHA --region=$AWS_DEFAULT_REGION \
     --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY \
     --timeout 600 \
     account-fixed-cluster account-celery-high-worker & \
-
 ecs deploy --tag=$COMMIT_SHA --region=$AWS_DEFAULT_REGION \
     --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY \
     --timeout 600 \
     account-fixed-cluster account-celery-low-worker & \
-
 wait $1 $2 $3
