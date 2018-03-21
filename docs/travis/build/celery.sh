ca782b2dc65b454f5c3ca72853a8f80086aa33c3
김정근 <snorlax@ridi.com>
2018-03-21 22:20:50 +0900

    Remove pre-build.sh

--- docs/travis/build/celery.sh
+++ docs/travis/build/celery.sh
@@ -2,6 +2,7 @@
 
 COMMIT_SHA=${TRAVIS_COMMIT::8}
 
+make ci-settings ns=$ENV access_key=$AWS_ACCESS_KEY_ID secret_key=$AWS_SECRET_ACCESS_KEY region=$AWS_DEFAULT_REGION
 make ci-build-celery env=$ENVIRONMENT
 
 # tag image
--- docs/travis/push/account.sh
+++ docs/travis/push/account.sh
@@ -2,5 +2,7 @@
 
 COMMIT_SHA=${TRAVIS_COMMIT::8}
 
+$(aws ecr get-login --no-include-email --region=$AWS_DEFAULT_REGION)
+
 make ci-push-account env=$ENVIRONMENT ecr_path=$ACCOUNT_ECR tag=latest
 make ci-push-account env=$ENVIRONMENT ecr_path=$ACCOUNT_ECR tag=$COMMIT_SHA
