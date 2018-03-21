9d05e8bf28e4dc4b7682f969f7beb9f63a5e99cc
김정근 <snorlax@ridi.com>
2018-03-21 22:37:38 +0900

    Remove build-stages

--- docs/travis/build/celery.sh
+++ docs/travis/build/celery.sh
@@ -2,7 +2,6 @@
 
 COMMIT_SHA=${TRAVIS_COMMIT::8}
 
-make ci-settings ns=$ENV access_key=$AWS_ACCESS_KEY_ID secret_key=$AWS_SECRET_ACCESS_KEY region=$AWS_DEFAULT_REGION
 make ci-build-celery env=$ENVIRONMENT
 
 # tag image
--- docs/travis/deploy.sh
+++ docs/travis/deploy.sh
@@ -2,6 +2,17 @@
 
 COMMIT_SHA=${TRAVIS_COMMIT::8}
 
+# deploy
+ecs deploy --tag=$COMMIT_SHA --region=$AWS_DEFAULT_REGION \
+    --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY \
+    -i account-nginx $ACCOUNT_ECR/$ENVIRONMENT/account/nginx-www:latest \
+    --timeout 600 \
+    account-scalable-cluster account-scalable & \
+ecs deploy --tag=$COMMIT_SHA --region=$AWS_DEFAULT_REGION \
+    --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY \
+    -i account-nginx $ACCOUNT_ECR/$ENVIRONMENT/account/nginx-admin:latest \
+    --timeout 600 \
+    account-fixed-cluster account-fixed & \
 ecs deploy --tag=$COMMIT_SHA --region=$AWS_DEFAULT_REGION \
     --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY \
     --timeout 600 \
@@ -14,4 +25,4 @@ ecs deploy --tag=$COMMIT_SHA --region=$AWS_DEFAULT_REGION \
     --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY \
     --timeout 600 \
     account-fixed-cluster account-celery-low-worker & \
-wait $1 $2 $3
+wait $1 $2 $3 $4 $5
