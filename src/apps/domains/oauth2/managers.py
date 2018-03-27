c5b3389c2be2ead508afd65fafef1501c96b6cd1
Junheong Park <jh.park@ridi.com>
2018-03-27 18:59:39 +0900

    lint 예외 추가 - manager에서 테이블 이름 확인을 위해 meta 데이터 접근

--- src/apps/domains/oauth2/managers.py
+++ src/apps/domains/oauth2/managers.py
@@ -6,13 +6,13 @@ from django.db.models import Manager
 
 class RefreshTokenManager(Manager):
     def revoke_by_expire_date(self, expire_date: datetime, limit):
-        table_name = self.model._meta.db_table
+        table_name = self.model._meta.db_table  # pylint:disable=protected-access
         with connection.cursor() as c:
             c.execute(f'DELETE FROM {table_name} WHERE expires < "{expire_date}" LIMIT {limit}')
 
 
 class GrantManager(Manager):
     def revoke_by_expire_date(self, expire_date: datetime, limit):
-        table_name = self.model._meta.db_table
+        table_name = self.model._meta.db_table  # pylint:disable=protected-access
         with connection.cursor() as c:
             c.execute(f'DELETE FROM {table_name} WHERE expires < "{expire_date}" LIMIT {limit}')
