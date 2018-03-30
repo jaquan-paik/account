4b6bfda9d279c5b45b4bde87bccc2b5ff6020888
김정근 <snorlax@ridi.com>
2018-03-30 17:33:33 +0900

    Fix memorize clear logic

--- src/lib/cache/memorize.py
+++ src/lib/cache/memorize.py
@@ -5,11 +5,8 @@ def memorize(func: Callable):
     _cache = {}
 
     def _decorator(*args, clear: bool=False, **kwargs):
-        if clear:
-            return func(*args, **kwargs)
-
         key = func.__name__ + str(args) + str(kwargs)
-        if key not in _cache:
+        if key not in _cache or clear:
             _cache[key] = func(*args, **kwargs)
         return _cache[key]
 
