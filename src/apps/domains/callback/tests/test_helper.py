f88c63eed46390cf9d80089b663c16f9ff5edf65
김정근 <snorlax@ridi.com>
2018-04-03 17:01:07 +0900

    Add TokenHelper TestCase

--- src/apps/domains/callback/tests/test_helper.py
+++ src/apps/domains/callback/tests/test_helper.py
@@ -1,13 +1,17 @@
+import requests_mock
 from django.core.exceptions import ObjectDoesNotExist
 from django.test import TestCase, RequestFactory
 from django.urls import reverse
 from django_dynamic_fixture import G
 
+from apps.domains.account.models import User
 from apps.domains.callback.constants import CookieRootDomains
+from apps.domains.callback.dtos import TokenData
 from apps.domains.callback.exceptions import NotAllowedRootDomainException
 from apps.domains.callback.helpers.client_helper import ClientHelper
+from apps.domains.callback.helpers.token_helper import TokenCodeHelper, TokenRefreshHelper
 from apps.domains.callback.helpers.url_helper import UrlHelper
-from apps.domains.oauth2.models import Application
+from apps.domains.oauth2.models import Application, RefreshToken
 from infra.configure.config import GeneralConfig
 
 
@@ -56,3 +60,44 @@ class ClientHelperTestCase(TestCase):
     def test_not_implemented(self):
         with self.assertRaises(NotImplementedError):
             ClientHelper.get_client(client_id=self.not_implement_client.client_id)
+
+
+class TokenHelperTestCase(TestCase):
+    def setUp(self):
+        self.client = G(Application, skip_authorization=True, user=None)
+
+    def test_token_code_helper(self):
+        with requests_mock.mock() as m:
+            m.post(UrlHelper.get_token(), json={
+                'access_token': 'test-access-token1111',
+                'expires_in': 1111111,
+                'refresh_token': 'test-refresh-token1111',
+                'refresh_token_expires_in': 2222222,
+            })
+            at, rt = TokenCodeHelper.get_tokens(client=self.client, code='test-code')
+
+            self.assertIsInstance(at, TokenData)
+            self.assertIsInstance(rt, TokenData)
+
+            self.assertEqual(at.token, 'test-access-token1111')
+            self.assertEqual(at.expires_in, 1111111)
+            self.assertEqual(rt.token, 'test-refresh-token1111')
+            self.assertEqual(rt.expires_in, 2222222)
+
+    def test_token_refresh_helper(self):
+        with requests_mock.mock() as m:
+            m.post(UrlHelper.get_token(), json={
+                'access_token': 'test-access-token2222',
+                'expires_in': 1111111,
+                'refresh_token': 'test-refresh-token2222',
+                'refresh_token_expires_in': 2222222,
+            })
+            at, rt = TokenRefreshHelper.get_tokens(client=self.client, code='test-refresh-token')
+
+            self.assertIsInstance(at, TokenData)
+            self.assertIsInstance(rt, TokenData)
+
+            self.assertEqual(at.token, 'test-access-token2222')
+            self.assertEqual(at.expires_in, 1111111)
+            self.assertEqual(rt.token, 'test-refresh-token2222')
+            self.assertEqual(rt.expires_in, 2222222)
