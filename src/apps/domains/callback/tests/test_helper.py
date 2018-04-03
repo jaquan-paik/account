3e0ddfebcf8704d806cf3c05eefaf3a7207e1c56
김정근 <snorlax@ridi.com>
2018-04-03 16:02:48 +0900

    Add ClientHelper TestCase

--- src/apps/domains/callback/tests/test_helper.py
+++ src/apps/domains/callback/tests/test_helper.py
@@ -1,9 +1,13 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.test import TestCase, RequestFactory
 from django.urls import reverse
+from django_dynamic_fixture import G
 
 from apps.domains.callback.constants import CookieRootDomains
 from apps.domains.callback.exceptions import NotAllowedRootDomainException
+from apps.domains.callback.helpers.client_helper import ClientHelper
 from apps.domains.callback.helpers.url_helper import UrlHelper
+from apps.domains.oauth2.models import Application
 from infra.configure.config import GeneralConfig
 
 
@@ -33,4 +37,22 @@ class UrlHelperTestCase(TestCase):
         request = factory.get('/', HTTP_HOST='dev.ridi.com')
 
         with self.assertRaises(NotAllowedRootDomainException):
-            _ = UrlHelper.get_root_domain(request)
+            UrlHelper.get_root_domain(request)
+
+
+class ClientHelperTestCase(TestCase):
+    def setUp(self):
+        self.client = G(Application, skip_authorization=True, user=None)
+        self.not_implement_client = G(Application, skip_authorization=True, user=None, authorization_grant_type="implicit")
+
+    def test_get_client(self):
+        client = ClientHelper.get_client(client_id=self.client.client_id)
+        self.assertEqual(client.client_id, self.client.client_id)
+
+    def test_not_exists_client(self):
+        with self.assertRaises(ObjectDoesNotExist):
+            ClientHelper.get_client(client_id='is_dummy')
+
+    def test_not_implemented(self):
+        with self.assertRaises(NotImplementedError):
+            ClientHelper.get_client(client_id=self.not_implement_client.client_id)
