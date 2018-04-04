0418f5607ba7614ca8c7bb2265775603dd50e79e
김정근 <snorlax@ridi.com>
2018-04-04 12:38:33 +0900

    Initialize testcase for callback view

---
+++ src/apps/domains/callback/tests/test_view.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+
+class InHouseCallbackTestCase(TestCase):
+    """
+    AuthorizeView 와 CallbackView를 함께 테스트한다.
+    """
+    pass
+
+
+class TokenViewTestCase(TestCase):
+    pass
+
+
+class LogoutViewTestCase(TestCase):
+    pass
