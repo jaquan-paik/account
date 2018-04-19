2bf36ab05409f736f72c457fc71d4e2298001c5b
Junheong Park <jh.park@ridi.com>
2018-04-19 21:40:22 +0900

    Remove unnessary fixed-cluster deploy command.

--- docs/travis/deploy.sh
+++ docs/travis/deploy.sh
@@ -4,8 +4,4 @@ COMMIT_SHA=${TRAVIS_COMMIT::8}
 
 # deploy
 ecs deploy --timeout=1200 --region=$AWS_DEFAULT_REGION --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY account-scalable-cluster account-scalable & \
-ecs deploy --timeout=1200 --region=$AWS_DEFAULT_REGION --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY account-fixed-cluster account-fixed & \
-ecs deploy --timeout=1200 --region=$AWS_DEFAULT_REGION --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY account-fixed-cluster account-celery-beat & \
-ecs deploy --timeout=1200 --region=$AWS_DEFAULT_REGION --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY account-fixed-cluster account-high-worker & \
-ecs deploy --timeout=1200 --region=$AWS_DEFAULT_REGION --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY account-fixed-cluster account-low-worker & \
 wait
