c10c1e931d583bbeb886394e3e985aec84ce4b6c
Junheong Park <jh.park@ridi.com>
2018-04-23 20:31:38 +0900

    Change development ECS cluster name and service name

--- docs/docker/compose/development/ecs_config.sh
+++ docs/docker/compose/development/ecs_config.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-ecs-cli configure --region ap-northeast-2 --cluster account-fixed-cluster
+ecs-cli configure --region ap-northeast-2 --cluster account-cluster
 
-ecs-cli compose --project-name account-celery-beat --file docker-compose-celery-beat.yml service up \
---deployment-min-healthy-percent 0 \
---deployment-max-percent 100
+#ecs-cli compose --project-name account-celery-beat --file docker-compose-celery-beat.yml service up \
+#--deployment-min-healthy-percent 0 \
+#--deployment-max-percent 100
+#
+#ecs-cli compose --project-name account-high-worker --file docker-compose-high-worker.yml service up \
+#--deployment-min-healthy-percent 100 \
+#--deployment-max-percent 200
+#
+#ecs-cli compose --project-name account-low-worker --file docker-compose-low-worker.yml service up \
+#--deployment-min-healthy-percent 100 \
+#--deployment-max-percent 200
 
-ecs-cli compose --project-name account-high-worker --file docker-compose-high-worker.yml service up \
---deployment-min-healthy-percent 100 \
---deployment-max-percent 200
-
-ecs-cli compose --project-name account-low-worker --file docker-compose-low-worker.yml service up \
---deployment-min-healthy-percent 100 \
---deployment-max-percent 200
-
-ecs-cli compose --project-name account-scalable --file docker-compose-www.yml service up \
+ecs-cli compose --project-name account-www --file docker-compose-www.yml service up \
 --deployment-min-healthy-percent 100 \
 --deployment-max-percent 200 \
 --target-group-arn arn:aws:elasticloadbalancing:ap-northeast-2:689221834431:targetgroup/ecs-account-scalable/34676c9b585f80ad \
