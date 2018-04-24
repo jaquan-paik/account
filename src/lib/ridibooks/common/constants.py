10614931fa769dfc7fcc8aa5ce8cf6e1fde683d1
김정근 <snorlax@ridi.com>
2018-04-24 12:14:52 +0900

    Add HttpMethod constants HttpMethod상수 추가

--- src/lib/ridibooks/common/constants.py
+++ src/lib/ridibooks/common/constants.py
@@ -1,3 +1,18 @@
-
+from lib.base.constants import BaseConstant
 
 HTTP_AUTHORIZATION_HEADER = 'HTTP_AUTHORIZATION'
+
+
+class HttpMethod(BaseConstant):
+    GET = 0
+    POST = 1
+    PUT = 2
+    DELETE = 4
+
+    _LIST = [GET, POST, PUT, DELETE]
+    _STRING_MAP = {
+        GET: 'GET',
+        POST: 'POST',
+        PUT: 'PUT',
+        DELETE: 'DELETE'
+    }
