6ce9a3de5fe40e19977b9f53a00820aa52e9d681
Junheong Park <jh.park@ridi.com>
2018-05-03 20:50:22 +0900

    Remove unused libraries

--- src/lib/email/managers.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class EmailBlacklistManager(models.Manager):
-    def is_in_blacklist(self, email: str) -> bool:
-        return self.filter(email=email, is_active=True).count() > 0
--- src/sites/base.py
+++ src/sites/base.py
@@ -23,7 +23,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lib.email.apps.EmailConfig',
     'rest_framework',
     'oauth2_provider',
     'corsheaders',
