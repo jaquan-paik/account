34f6d6ac3f008218689f96223d5dd99659d3b829
Junheong Park <jh.park@ridi.com>
2018-05-03 11:18:48 +0900

    change aws elb target group arn

--- docs/docker/compose/production/ecs_config.sh
+++ docs/docker/compose/production/ecs_config.sh
@@ -5,7 +5,7 @@ ecs-cli configure --region ap-northeast-2 --cluster account-cluster
 ecs-cli compose --project-name account-www --file docker-compose-www.yml service up \
 --deployment-min-healthy-percent 100 \
 --deployment-max-percent 200 \
---target-group-arn arn:aws:elasticloadbalancing:ap-northeast-2:588135293606:targetgroup/Account/cfec983008611061 \
+--target-group-arn arn:aws:elasticloadbalancing:ap-northeast-2:588135293606:targetgroup/account-www/5cc15ab261b2021f \
 --container-name account-nginx \
 --container-port 80 \
 --role ecsServiceRole
