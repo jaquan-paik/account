3309c41d52b5001abcba7796a9ee934cc208629b
최아연 <ayon.c@ridi.com>
2018-05-14 20:49:12 +0900

    594fa05(added TinyIntegerField, PositiveTinyIntegerField)

--- src/lib/django/db/mysql.py
+++ src/lib/django/db/mysql.py
@@ -1,8 +1,25 @@
+from django.db import models
 from django.db.backends.mysql.features import DatabaseFeatures
 from django.utils.functional import cached_property
+
+MYSQL_ENGINE = 'django.db.backends.mysql'
 
 
 class MysqlDatabaseFeatures(DatabaseFeatures):
     @cached_property
     def supports_microsecond_precision(self):
         return False
+
+
+class TinyIntegerField(models.SmallIntegerField):
+    def db_type(self, connection):
+        if connection.settings_dict['ENGINE'] == MYSQL_ENGINE:
+            return "tinyint"
+        return super(TinyIntegerField, self).db_type(connection)
+
+
+class PositiveTinyIntegerField(models.PositiveSmallIntegerField):
+    def db_type(self, connection):
+        if connection.settings_dict['ENGINE'] == MYSQL_ENGINE:
+            return "tinyint unsigned"
+        return super(PositiveTinyIntegerField, self).db_type(connection)
