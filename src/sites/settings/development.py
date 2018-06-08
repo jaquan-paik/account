875b8c72636d2e0053d30db895b9aafed5cd8d7c
Junheong Park <jh.park@ridi.com>
2018-06-08 15:43:50 +0900

    Allow all origin about CORS to test.

--- src/sites/settings/development.py
+++ src/sites/settings/development.py
@@ -11,8 +11,9 @@ setup_logging()
 SITE_DOMAIN = 'account.dev.ridi.io'
 ALLOWED_HOSTS = [SITE_DOMAIN, 'account.dev.ridi.com', 'dev.ridi.com', 'dev.ridi.io']
 
-CORS_ORIGIN_REGEX_WHITELIST = (r'^(https?://)?(\w+\.)?dev\.ridi\.com$', r'^(https?://)?(\w+\.)?dev\.ridi\.io$')
-CORS_URLS_REGEX = r'^/ridi/.*$'
+CORS_ORIGIN_ALLOW_ALL = False
+# CORS_ORIGIN_REGEX_WHITELIST = (r'^(https?://)?(\w+\.)?dev\.ridi\.com$', r'^(https?://)?(\w+\.)?dev\.ridi\.io$')
+# CORS_URLS_REGEX = r'^/ridi/.*$'
 
 RIDIBOOKS_LOGIN_URL = 'https://dev.ridi.io/account/login'
 
