8fcbc5ae30a1ceb2e5ddddad1b56a2c0e9439e25
Junheong Park <jh.park@ridi.com>
2018-06-11 20:05:12 +0900

    Test CORS_URLS_REGEX for all.

--- src/sites/settings/production.py
+++ src/sites/settings/production.py
@@ -3,12 +3,9 @@ from lib.log.setup import setup_logging
 # noinspection PyUnresolvedReferences
 from .base import *  # flake8: noqa: F403  # pylint:disable=wildcard-import
 
-# noinspection PyUnresolvedReferences
 setup_logging()
 
 SITE_DOMAIN = 'account.ridibooks.com'
-
-# noinspection PyUnresolvedReferences
 ALLOWED_HOSTS = [SITE_DOMAIN, 'ridibooks.com', ]
 
 CORS_ORIGIN_REGEX_WHITELIST = (r'^(https?://)?(\w+\.)?ridibooks\.com$', )
