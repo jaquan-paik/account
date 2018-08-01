71a171fc228f36ee593fbff60ef9f61a94ebfeb1
Junheong Park <jh.park@ridi.com>
2018-08-01 12:18:19 +0900

    Change allow root domain in dev. - to ridi.io

--- src/apps/domains/callback/constants.py
+++ src/apps/domains/callback/constants.py
@@ -8,19 +8,21 @@ ROOT_DOMAIN_SESSION_KEY = 'root_domain'
 class CookieRootDomains(BaseConstant):
     DEV_RIDI_COM = 0
     DEV_RIDI_IO = 1
+    RIDI_IO = 2
 
-    PROD_RIDI_COM = 2
+    PROD_RIDI_COM = 10
 
-    _LIST = [DEV_RIDI_COM, DEV_RIDI_IO, PROD_RIDI_COM]
+    _LIST = [DEV_RIDI_COM, RIDI_IO, DEV_RIDI_IO, PROD_RIDI_COM]
     _STRING_MAP = {
         DEV_RIDI_COM: 'dev.ridi.com',
         DEV_RIDI_IO: 'dev.ridi.io',
+        RIDI_IO: 'ridi.io',
         PROD_RIDI_COM: 'ridibooks.com'
     }
 
     @classmethod
     def get_root_whitelist(cls, debug: bool) -> typing.List[str]:
         if debug:
-            return [cls.to_string(cls.DEV_RIDI_COM), cls.to_string(cls.DEV_RIDI_IO)]
+            return [cls.to_string(cls.DEV_RIDI_COM), cls.to_string(cls.RIDI_IO)]
 
         return [cls.to_string(cls.PROD_RIDI_COM)]
