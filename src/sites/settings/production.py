5a439503210b689e6d368d58e7fce48034495cd2
김정근 <snorlax@ridi.com>
2018-08-27 12:57:48 +0900

    Add ezwel.ridibooks.com to corl urls1

--- src/sites/settings/production.py
+++ src/sites/settings/production.py
@@ -6,7 +6,11 @@ from .base import *  # flake8: noqa: F403  # pylint:disable=wildcard-import
 setup_logging(Secret().get(SecretKeyName.SENTRY_DSN))
 
 SITE_DOMAIN = 'account.ridibooks.com'
-ALLOWED_HOSTS = [SITE_DOMAIN, 'ridibooks.com', ]
+ALLOWED_HOSTS = [
+    SITE_DOMAIN,
+    'ridibooks.com',
+    'ezwel.ridibooks.com'
+]
 
 STORE_URL = 'https://ridibooks.com'
 RIDIBOOKS_LOGIN_URL = 'https://ridibooks.com/account/login'
