6345c22be0632f616b9da09bc554522b883f7a7e
Junheong Park <jh.park@ridi.com>
2018-10-19 14:20:02 +0900

    Test CSP

--- src/sites/settings/base.py
+++ src/sites/settings/base.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'csp.middleware.CSPMiddleware',
+    # 'csp.middleware.CSPMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'lib.ridibooks.store_auth.middlewares.AuthenticationMiddleware',
