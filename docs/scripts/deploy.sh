2e2e309233d47067355278f22f963d22e99b0de8
Junheong Park <jh.park@ridi.com>
2018-10-31 16:48:50 +0900

    docker-compose 로 이미지 빌드하도록 변경

--- docs/scripts/deploy.sh
+++ docs/scripts/deploy.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 set -e
 
+run_and_check_exit_code() {
+    pids=""
+    for command in "$@"; do
+        eval $command &
+        pids="$pids $!"
+    done
+
+    for p in $pids; do
+        if wait $p; then
+            echo "success"
+        else
+            echo "failure"
+            exit 1
+        fi
+    done
+}
+
+
 # Set environment
 if [ "$1" = development ]; then
 
@@ -8,7 +26,7 @@ export ENVIRONMENT=development
 export ACCOUNT_ECR=$DEV_ACCOUNT_ECR
 export AWS_ACCESS_KEY_ID=$DEV_AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$DEV_AWS_SECRET_ACCESS_KEY
-export IMAGE_TAG=${TRAVIS_COMMIT::8}
+export TAG=${TRAVIS_COMMIT::8}
 
 elif [ "$1" = staging ]; then
 
@@ -16,7 +34,7 @@ export ENVIRONMENT=staging
 export ACCOUNT_ECR=$STAGING_ACCOUNT_ECR
 export AWS_ACCESS_KEY_ID=$PROD_AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$PROD_AWS_SECRET_ACCESS_KEY
-export IMAGE_TAG=${TRAVIS_COMMIT::8}
+export TAG=${TRAVIS_COMMIT::8}
 
 else
 
@@ -24,9 +42,10 @@ export ENVIRONMENT=production
 export ACCOUNT_ECR=$PROD_ACCOUNT_ECR
 export AWS_ACCESS_KEY_ID=$PROD_AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$PROD_AWS_SECRET_ACCESS_KEY
-export IMAGE_TAG=$TRAVIS_TAG
+export TAG=$TRAVIS_TAG
 
 fi
+
 
 # Prepare build
 make ci-settings ns=$ENVIRONMENT
@@ -39,29 +58,27 @@ wait
 
 
 # Build image
-docker build -t $ENVIRONMENT/account/www:latest -f ./docs/docker/account/Dockerfile . --build-arg ENVIRONMENT="$ENVIRONMENT" & \
-docker build -t $ENVIRONMENT/account/nginx:latest -f ./docs/docker/nginx/Dockerfile . --build-arg ENVIRONMENT="$ENVIRONMENT" & \
-wait
+docker-compose -f ./docs/docker/compose/build.yml build
 
-# Tag image
-docker tag $ENVIRONMENT/account/www:latest $ACCOUNT_ECR/$ENVIRONMENT/account/www:latest & \
-docker tag $ENVIRONMENT/account/www:latest $ACCOUNT_ECR/$ENVIRONMENT/account/www:$IMAGE_TAG & \
-docker tag $ENVIRONMENT/account/www:latest $ACCOUNT_ECR/$ENVIRONMENT/account/nginx:latest & \
-docker tag $ENVIRONMENT/account/www:latest $ACCOUNT_ECR/$ENVIRONMENT/account/nginx:$IMAGE_TAG & \
-wait
 
 # Push image
-docker push $ACCOUNT_ECR/$ENVIRONMENT/account/www:latest & \
-docker push $ACCOUNT_ECR/$ENVIRONMENT/account/www:$IMAGE_TAG & \
-docker push $ACCOUNT_ECR/$ENVIRONMENT/account/nginx:latest & \
-docker push $ACCOUNT_ECR/$ENVIRONMENT/account/nginx:$IMAGE_TAG & \
-wait
+run_and_check_exit_code "ecs-cli push $ENVIRONMENT/account/uwsgi:$TAG"
+run_and_check_exit_code "ecs-cli push $ENVIRONMENT/library_api/nginx:$TAG"
+
+if [ $? -ne 0 ]; then
+    echo "push fails"
+    exit 1
+fi
 
 
 # Deploy
-ecs deploy --timeout=1200 --region=$AWS_DEFAULT_REGION --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY account-cluster account-www & \
-ecs deploy --timeout=1200 --region=$AWS_DEFAULT_REGION --access-key-id=$AWS_ACCESS_KEY_ID --secret-access-key=$AWS_SECRET_ACCESS_KEY account-cluster account-cron & \
-wait
+run_and_check_exit_code "ecs-cli compose --cluster account-cluster --project-name account-www -f docs/docker/compose/account.yml service up" \
+                        "ecs-cli compose --cluster account-cluster --project-name account-cron -f docs/docker/compose/cron.yml service up"
+
+if [ $? -ne 0 ]; then
+    echo "deploy fails"
+    exit 1
+fi
 
 
 curl -X POST --data-urlencode "payload={\"text\": \"[$ENVIRONMENT - $IMAGE_TAG] 계정서버 배포가 완료되었습니다.\nRepo: https://github.com/ridi/account\"}" $SLACK_DEPLOY_HOOK
