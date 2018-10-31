291d5853a8dc413dca0ed2b7cabfc7da82edbc38
Junheong Park <jh.park@ridi.com>
2018-10-31 18:55:13 +0900

    Deploy slack 메세지에 Tag 올바르게 나오도록 함

--- docs/scripts/deploy.sh
+++ docs/scripts/deploy.sh
@@ -81,4 +81,4 @@ if [ $? -ne 0 ]; then
 fi
 
 
-curl -X POST --data-urlencode "payload={\"text\": \"[$ENVIRONMENT - $IMAGE_TAG] 계정서버 배포가 완료되었습니다.\nRepo: https://github.com/ridi/account\"}" $SLACK_DEPLOY_HOOK
+curl -X POST --data-urlencode "payload={\"text\": \"[$ENVIRONMENT - $TAG] 계정서버 배포가 완료되었습니다.\nRepo: https://github.com/ridi/account\"}" $SLACK_DEPLOY_HOOK
