9478e56b2015efd21405882a870b0818592f66c8
Junheong Park <jh.park@ridi.com>
2018-11-01 16:09:06 +0900

    Staging 배포 테스트

--- docs/scripts/deploy.sh
+++ docs/scripts/deploy.sh
@@ -71,18 +71,18 @@ if [ $? -ne 0 ]; then
 fi
 
 
-## Deploy
-#if [ "$1" = staging ]; then
-#run_and_check_exit_code "ecs-cli compose --cluster account-cluster --project-name account-www -f docs/docker/compose/account.yml service up --timeout 1200"
-#else
-#run_and_check_exit_code "ecs-cli compose --cluster account-cluster --project-name account-www -f docs/docker/compose/account.yml service up --timeout 1200" \
-#                        "ecs-cli compose --cluster account-cluster --project-name account-cron -f docs/docker/compose/cron.yml service up --timeout 1200"
-#fi
-#
-#if [ $? -ne 0 ]; then
-#    echo "deploy fails"
-#    exit 1
-#fi
+# Deploy
+if [ "$1" = staging ]; then
+run_and_check_exit_code "ecs-cli compose --cluster account-cluster --project-name account-www -f docs/docker/compose/account.yml service up --timeout 1200"
+else
+run_and_check_exit_code "ecs-cli compose --cluster account-cluster --project-name account-www -f docs/docker/compose/account.yml service up --timeout 1200" \
+                        "ecs-cli compose --cluster account-cluster --project-name account-cron -f docs/docker/compose/cron.yml service up --timeout 1200"
+fi
+
+if [ $? -ne 0 ]; then
+    echo "deploy fails"
+    exit 1
+fi
 
 
 curl -X POST --data-urlencode "payload={\"text\": \"[Account][$ENVIRONMENT - $TAG] 배포가 완료되었습니다.\nRepo: https://github.com/ridi/account\"}" $SLACK_DEPLOY_HOOK
