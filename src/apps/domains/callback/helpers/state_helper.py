8121548f15ac245d5117fb519b8dc9b0bff726a4
seungmin paik <sm.paik@ridi.com>
2018-12-10 11:22:54 +0900

    stateHelper 구현 추가

--- src/apps/domains/callback/helpers/state_helper.py
+++ src/apps/domains/callback/helpers/state_helper.py
@@ -6,23 +6,27 @@ from lib.secret.secret import Secret
 from lib.crypto.encrypt import CryptoHelper
 import json
 
-CRYPTO_KEY = '!Ck[v%W}$5,4@-5R'
+CRYPTO_KEY = Secret().get(SecretKeyName.SECRET_KEY)[:32]
+EXPIRE_TIME = 10
 
 
 class StateHelper:
     @staticmethod
     def create_encrypted_state(u_idx: str) -> str:
-        state = {'datetime': str(datetime.now()), 'u_idx': u_idx}
-        # return CryptoHelper(Secret().get(SecretKeyName.SECRET_KEY)).encrypt(json.dumps(state))
+        state = {'time': datetime.now().timestamp(), 'u_idx': u_idx}
         return CryptoHelper(CRYPTO_KEY).encrypt(json.dumps(state))
 
     @staticmethod
-    def _decrypt_state(state: str) -> str:
-        return CryptoHelper(CRYPTO_KEY).decrypt(state)
+    def _decrypt_state(state: str) -> dict:
+        decrypted_data = json.loads(CryptoHelper(CRYPTO_KEY).decrypt(state))
+        if not decrypted_data:
+            raise PermissionDenied()
+        return decrypted_data
 
     @classmethod
     def validate_state(cls, state: str, u_idx: str):
-        decrypted_state = cls._decrypt_state(state)
-        from lib.log.logger import logger
-        logger.info(decrypted_state)
-        # raise PermissionDenied()
+        decrypted_data = cls._decrypt_state(state)
+        if decrypted_data['u_idx'] != u_idx:
+            raise PermissionDenied()
+        if decrypted_data['time'] + EXPIRE_TIME < datetime.now().timestamp():
+            raise PermissionDenied()
