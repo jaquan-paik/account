b82b978c2b3c782f26a705d1c76d2adb0597f97e
seungmin paik <sm.paik@ridi.com>
2018-12-19 15:09:19 +0900

    add token cookie view파트로 이동

--- src/apps/domains/ridi/views.py
+++ src/apps/domains/ridi/views.py
@@ -5,6 +5,7 @@ from drf_yasg.utils import swagger_auto_schema
 from rest_framework.views import APIView
 
 from apps.domains.ridi.helpers.client_helper import ClientHelper
+from apps.domains.ridi.helpers.response_cookie_helper import ResponseCookieHelper
 from apps.domains.ridi.helpers.token_request_helper import TokenRequestHelper
 from apps.domains.ridi.helpers.token_helper import TokenHelper
 from apps.domains.ridi.helpers.url_helper import UrlHelper
@@ -16,7 +17,7 @@ from apps.domains.oauth2.token import JwtHandler
 from apps.domains.ridi.forms import AuthorizeForm, CallbackForm, TokenForm
 
 from infra.network.constants.http_status_code import HttpStatusCodes
-from lib.decorators.cookie_handler import add_tokens_in_cookie, clear_tokens_in_cookie, EMPTY_TOKENS
+from lib.decorators.cookie_handler import clear_tokens_in_cookie
 from lib.decorators.exception_handler import exception_handler
 
 
@@ -29,7 +30,6 @@ class AuthorizeView(LoginRequiredMixin, View):
 
 class CallbackView(View):
     @exception_handler
-    @add_tokens_in_cookie
     def get(self, request):
         # TODO : ------ 재배포시 삭제 시작 부분 ------
         deprecated = request.GET.get('deprecated', None)
@@ -37,21 +37,23 @@ class CallbackView(View):
         client_id = request.GET.get('client_id', None)
         in_house_redirect_uri = request.GET.get('in_house_redirect_uri', None)
         if deprecated:
-            tokens = TokenRequestHelper.get_tokens(
+            access_token, refresh_token, = TokenRequestHelper.get_tokens(
                 grant_type='authorization_code', client=ClientHelper.get_in_house_client(client_id),
                 code=code, redirect_uri=f'{UrlHelper.get_redirect_url(in_house_redirect_uri, client_id)}&deprecated=1'
             )
 
             response = InHouseHttpResponseRedirect(in_house_redirect_uri)
-            return response, tokens
+            ResponseCookieHelper.add_token_cookie(response, access_token, refresh_token, UrlHelper.get_root_domain(request))
+            return response
         # TODO : ------- 재배포시 삭제 -------
 
         valid_data = CallbackForm(request.GET).get_valid_data_with_state_check(request.user.idx)
-        tokens = AuthorizationCodeService.get_tokens(
+        access_token, refresh_token, = AuthorizationCodeService.get_tokens(
             valid_data['code'], valid_data['client_id'], valid_data['in_house_redirect_uri']
         )
         response = InHouseHttpResponseRedirect(valid_data['in_house_redirect_uri'])
-        return response, tokens
+        ResponseCookieHelper.add_token_cookie(response, access_token, refresh_token, UrlHelper.get_root_domain(request))
+        return response
 
 
 class CompleteView(View):
@@ -62,19 +64,19 @@ class CompleteView(View):
 class TokenView(APIView):
     @swagger_auto_schema(**TokenGetSchema.to_swagger_schema())
     @exception_handler
-    @add_tokens_in_cookie
     def post(self, request):
         valid_data = TokenForm(request.COOKIES).get_valid_data()
         access_token = JwtHandler.get_access_token(valid_data['access_token'])
         if not access_token:
-            tokens = TokenRefreshService.get_tokens(valid_data['refresh_token'])
-            data = TokenHelper.get_token_data_info(tokens[0])
-
+            access_token, refresh_token = TokenRefreshService.get_tokens(valid_data['refresh_token'])
+            data = TokenHelper.get_token_data_info(access_token)
+            response = JsonResponse(data)
+            ResponseCookieHelper.add_token_cookie(response, access_token, refresh_token, UrlHelper.get_root_domain(request))
         else:
-            tokens = EMPTY_TOKENS
             data = TokenHelper.get_token_info(access_token)
+            response = JsonResponse(data)
 
-        return JsonResponse(data), tokens
+        return response
 
 
 class LogoutView(View):
--- src/lib/decorators/cookie_handler.py
+++ src/lib/decorators/cookie_handler.py
@@ -2,19 +2,6 @@ from typing import Callable
 from apps.domains.ridi.helpers.response_cookie_helper import ResponseCookieHelper
 from apps.domains.ridi.helpers.url_helper import UrlHelper
 
-EMPTY_TOKENS = (False, False)
-
-
-def add_tokens_in_cookie(func: Callable):
-    def wrapper(self, request, *args, **kwargs):
-        response, (access_token, refresh_token) = func(self, request, *args, **kwargs)
-        root_domain = UrlHelper.get_root_domain(request)
-        if access_token or refresh_token:
-            ResponseCookieHelper.add_token_cookie(response, access_token, refresh_token, root_domain)
-        return response
-
-    return wrapper
-
 
 def clear_tokens_in_cookie(func: Callable):
     def wrapper(self, request, *args, **kwargs):
