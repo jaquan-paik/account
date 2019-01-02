f9d1a4b61b445a84e82485ba80a9891f61fee6cb
seungmin paik <sm.paik@ridi.com>
2019-01-03 08:33:01 +0900

    deprecated 호환성 코드 삭제

--- src/apps/domains/oauth2/models.py
+++ src/apps/domains/oauth2/models.py
@@ -6,7 +6,6 @@ from apps.domains.oauth2.constants import JwtAlg
 from apps.domains.oauth2.managers import ApplicationManager, GrantManager, RefreshTokenManager
 from lib.django.db.mysql import TinyBooleanField
 from lib.utils.string import generate_random_str
-from lib.utils.url import get_url_until_path
 
 JWT_HS_256_SECRET_LEN = 32
 
@@ -56,10 +55,6 @@ class Grant(AbstractGrant):
     updated = None
     created = models.DateTimeField(auto_now_add=True, editable=False, verbose_name='등록일')
     last_modified = models.DateTimeField(auto_now=True, editable=False, verbose_name='수정일')
-
-    # TODO: 후에 삭제
-    def redirect_uri_allowed(self, uri):
-        return get_url_until_path(self.redirect_uri) == get_url_until_path(uri)
 
     objects = GrantManager()
 
