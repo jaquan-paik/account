84871d78e51ca9112423641f683f1bbc3c6a2d1b
seungmin paik <sm.paik@ridi.com>
2019-01-02 09:41:19 +0900

    setting_path 재설정

--- src/manage.py
+++ src/manage.py
@@ -9,8 +9,12 @@ from lib.secret.secret import Secret
 if __name__ == "__main__":
     # copy system arguments
     arguments = sys.argv[:]
-    setting_path = f'sites.settings.{Secret().get(SecretKeyName.ENVIRONMENT)}'
+    if Secret().get(SecretKeyName.ENVIRONMENT) == 'development':
+        setting_path = f'sites.settings.{Secret().get(SecretKeyName.ENVIRONMENT)}'
+    else:
+        setting_path = 'sites.settings.base'
 
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', setting_path)
     from django.core.management import execute_from_command_line
+
     execute_from_command_line(arguments)
