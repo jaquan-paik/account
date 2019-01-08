7dd3e40fa5db9b235cd6e1f34b1578d7417c8594
seungmin paik <sm.paik@ridi.com>
2019-01-08 10:32:01 +0900

    parameter type 기입 및 변수 명 수정

--- src/apps/domains/ridi/helpers/response_cookie_helper.py
+++ src/apps/domains/ridi/helpers/response_cookie_helper.py
@@ -1,3 +1,5 @@
+from typing import Optional
+from datetime import datetime
 from apps.domains.ridi.dtos import TokenData
 from lib.ridibooks.common.constants import ACCESS_TOKEN_COOKIE_KEY, REFRESH_TOKEN_COOKIE_KEY
 
@@ -9,19 +11,16 @@ class ResponseCookieHelper:
     def add_token_cookie(cls, response, access_token: TokenData, refresh_token: TokenData, root_domain: str):
         cls.set_secure_cookie(
             response, key=ACCESS_TOKEN_COOKIE_KEY, value=access_token.token, domain=root_domain,
-            expires_in=access_token.expires_in, expires_date=access_token.cookie_expire_time,
+            max_age=access_token.expires_in, expires=access_token.cookie_expire_time,
         )
         cls.set_secure_cookie(
             response, key=REFRESH_TOKEN_COOKIE_KEY, value=refresh_token.token, domain=root_domain,
-            expires_in=refresh_token.expires_in, expires_date=refresh_token.cookie_expire_time,
+            max_age=refresh_token.expires_in, expires=refresh_token.cookie_expire_time,
         )
 
     @staticmethod
-    def set_secure_cookie(response, key: str, value: str, domain: str, **kwargs):
-        response.set_cookie(
-            key, value, max_age=kwargs.get('expires_in', None), expires=kwargs.get('expires_date', None),
-            domain=domain, secure=True, httponly=True
-        )
+    def set_secure_cookie(response, key: str, value: str, domain: str, max_age: Optional[int] = None, expires: Optional[datetime] = None):
+        response.set_cookie(key, value, max_age=max_age, expires=expires, domain=domain, secure=True, httponly=True)
 
     @staticmethod
     def clear_cookie(response, key: str, domain: str):
