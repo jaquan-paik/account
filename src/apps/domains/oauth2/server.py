baf4005f8be6b7ee876097284647e6e37b927ac3
seungmin paik <sm.paik@ridi.com>
2019-01-10 18:50:34 +0900

    lint disable 추가

--- src/apps/domains/oauth2/server.py
+++ src/apps/domains/oauth2/server.py
@@ -4,6 +4,7 @@ from oauthlib.oauth2.rfc6749.tokens import random_token_generator
 from apps.domains.oauth2.token import JwtHandler
 
 
+# pylint:disable=too-many-ancestors
 class RidiServer(Server):
     def __init__(self, *args, **kwargs):
         super().__init__(token_generator=JwtHandler.generate, refresh_token_generator=random_token_generator, *args, **kwargs)
