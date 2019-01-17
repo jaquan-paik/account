5ac64f326bd101a12c99dfae0b67507ce6d37e68
seungmin paik <sm.paik@ridi.com>
2019-01-17 09:37:08 +0900

    불필요한 json dump 제거

--- src/lib/base/invalid_form_response.py
+++ src/lib/base/invalid_form_response.py
@@ -2,11 +2,10 @@ from django.forms import Form
 from django.shortcuts import render
 from infra.network.constants.http_status_code import HttpStatusCodes
 
-import json
+# import json
 
 
 def invalid_form_response(request, form: Form):
-    data = json.dumps(form.errors, ensure_ascii=False)
-    response = render(request, 'www/error/invalid_form.html', {'message': data})
+    response = render(request, 'www/error/invalid_form.html', {'message': form.errors})
     response.status_code = HttpStatusCodes.C_400_BAD_REQUEST
     return response
