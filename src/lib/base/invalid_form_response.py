5565d24aea45ef689bb3a22eaa7c0ea945cabcc1
seungmin paik <sm.paik@ridi.com>
2019-01-17 10:01:37 +0900

    invalid form 템플릿 추가

--- src/lib/base/invalid_form_response.py
+++ src/lib/base/invalid_form_response.py
@@ -2,8 +2,6 @@ from django.forms import Form
 from django.shortcuts import render
 from infra.network.constants.http_status_code import HttpStatusCodes
 
-# import json
-
 
 def invalid_form_response(request, form: Form):
     response = render(request, 'www/error/invalid_form.html', {'message': form.errors})
