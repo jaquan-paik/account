eb677e0d68fabc13eaec78d70c23c2f7df9e1848
seungmin paik <sm.paik@ridi.com>
2019-03-18 17:28:08 +0900

    redirect 하도록 변경

--- src/apps/domains/oauth2/views.py
+++ src/apps/domains/oauth2/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseRedirect
 from django.views import View
 
 from apps.domains.oauth2.forms import AuthorizationForm
 from apps.domains.oauth2.services.oauth2_authorization_code_service import OAuth2AuthorizationCodeService
 from apps.domains.ridi.response import get_template_from_form_error
+from lib.utils.url import generate_query_url
 
 
 class AuthorizationView(LoginRequiredMixin, View):
@@ -14,10 +15,10 @@ class AuthorizationView(LoginRequiredMixin, View):
             return get_template_from_form_error(request, authorize_form.errors)
         cleaned_data = authorize_form.clean()
         code = OAuth2AuthorizationCodeService.create_code(cleaned_data['client_id'], cleaned_data['redirect_uri'], request.user.idx)
-        #
-        # redirect_param = {'code': code}
-        # if cleaned_data['state']:
-        #     redirect_param['state'] = cleaned_data['state']
-        #
-        # redirect_uri = generate_query_url(cleaned_data['redirect_uri'], redirect_param)
-        return JsonResponse(data={'code': code}, status=200)
+
+        redirect_param = {'code': code}
+        if cleaned_data['state']:
+            redirect_param['state'] = cleaned_data['state']
+
+        redirect_uri = generate_query_url(cleaned_data['redirect_uri'], redirect_param)
+        return HttpResponseRedirect(redirect_uri)
