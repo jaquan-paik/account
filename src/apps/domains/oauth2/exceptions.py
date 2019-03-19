c96d215b7ba23db69de830ba4e0f733edc8b1d74
seungmin paik <sm.paik@ridi.com>
2019-03-19 09:53:18 +0900

    oauth2 error 영어로 변경

--- src/apps/domains/oauth2/exceptions.py
+++ src/apps/domains/oauth2/exceptions.py
@@ -8,22 +8,22 @@ class JwtTokenErrorException(Exception):
 class LoginFailError(OAuth2Error):
     error = 'login_fail'
     status_code = 401
-    description = '로그인에 실패했습니다.'
+    description = 'Login failed'
 
 
 class InvalidUserError(OAuth2Error):
     error = 'invalid_user'
     status_code = 401
-    description = '인증에 실패했습니다.'
+    description = 'Authentication failed.'
 
 
 class InvalidUserUnverifiedError(InvalidUserError):
-    description = '인증이 완료되지 않은 계정입니다.'
+    description = 'This account has not been verified.'
 
 
 class InvalidUserSecededError(InvalidUserError):
-    description = '탈퇴된 계정입니다.'
+    description = 'This account is unsubscribed.'
 
 
 class InvalidUserDormantedError(InvalidUserError):
-    description = '휴면 계정입니다.'
+    description = 'This account is Inactive.'
