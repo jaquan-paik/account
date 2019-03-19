9270ea95bfc4e2cc5bee846e603ecde4e7ef26e9
seungmin paik <sm.paik@ridi.com>
2019-03-19 10:16:40 +0900

    form 에서 걸리는 에러 메세지 영어로 변경

--- src/apps/domains/oauth2/fields.py
+++ src/apps/domains/oauth2/fields.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from apps.domains.oauth2.constants import ResponseType, ErrorMessage
+from apps.domains.oauth2.constants import ResponseType
 
 
 # pylint:disable=arguments-differ
@@ -8,18 +8,18 @@ from apps.domains.oauth2.constants import ResponseType, ErrorMessage
 class ClientIdField(forms.CharField):
     def validate(self, client_id):
         if not client_id:
-            raise forms.ValidationError('client_id는 필수 값입니다.', ErrorMessage.REQUIRED_CLIENT_ID)
+            raise forms.ValidationError('client id is required')
 
 
 class ResponseTypeCodeField(forms.CharField):
     def validate(self, response_type):
         if not response_type:
-            raise forms.ValidationError('response_type은 필수 값입니다.', ErrorMessage.REQUIRED_RESPONSE_TYPE)
+            raise forms.ValidationError('response_type is required')
         if response_type != ResponseType.CODE:
-            raise forms.ValidationError('지원되지 않는 response_type 입니다.', ErrorMessage.UNSUPPORTED_RESPONSE_TYPE)
+            raise forms.ValidationError('unsupported response_type')
 
 
 class RedirectUriField(forms.URLField):
     def validate(self, redirect_uri):
         if not redirect_uri:
-            raise forms.ValidationError('redirect_uri는 필수 값입니다.', ErrorMessage.REQUIRED_REDIRECT_URI)
+            raise forms.ValidationError('redirect_uri is required')
