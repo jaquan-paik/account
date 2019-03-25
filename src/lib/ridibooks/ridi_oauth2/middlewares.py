ab49fef5080230cd528ee1e0919020a2276cfce4
seungmin paik <sm.paik@ridi.com>
2019-03-25 10:09:22 +0900

    token login 에 user default를 annonymouseuser로 설정

--- src/lib/ridibooks/ridi_oauth2/middlewares.py
+++ src/lib/ridibooks/ridi_oauth2/middlewares.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
 from django.utils.deprecation import MiddlewareMixin
 
 from ridi_django_oauth2.utils.token import get_token_from_cookie, get_token_info
@@ -6,6 +7,7 @@ from ridi_django_oauth2.utils.token import get_token_from_cookie, get_token_info
 
 class AuthenticationMiddleware(MiddlewareMixin):
     def process_request(self, request):
+        request.user = AnonymousUser()
         token = get_token_from_cookie(request=request)
         token_info = None
         if token.access_token:
