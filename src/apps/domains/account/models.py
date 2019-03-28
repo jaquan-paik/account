d4e2c5728cbb89d50970ede3bc7518662495b8bb
seungmin paik <sm.paik@ridi.com>
2019-03-28 10:43:51 +0900

    user model column 추가 및 EqualizeMixin 추가

--- src/apps/domains/account/models.py
+++ src/apps/domains/account/models.py
@@ -1,14 +1,30 @@
 from django.db import models
 
+from apps.domains.account.constants import GenderType, StatusType
 from apps.domains.account.managers import UserManager
-from lib.django.db.models.base_model import BaseModel, BaseUserModel
+from lib.django.db.models.base_model import BaseModel, BaseUserModel, EqualizeMixin
 
 
-class User(BaseUserModel):
+class User(EqualizeMixin, BaseUserModel):
     idx = models.AutoField(primary_key=True, editable=False, verbose_name='u_idx')
     id = models.CharField(max_length=32, unique=True, editable=False, verbose_name='u_id', )
 
+    name = models.CharField(null=True, max_length=32, verbose_name='name')
+    reg_date = models.DateTimeField(null=True, verbose_name='reg_date')
+    ip = models.CharField(null=True, max_length=16, verbose_name='ip')
+    device_id = models.CharField(null=True, max_length=128, verbose_name='device_id')
+    email = models.CharField(null=True, max_length=256, verbose_name='email')
+    birth_date = models.DateTimeField(null=True, verbose_name='birth_date')
+    gender = models.IntegerField(null=True, choices=GenderType.get_choices(), verbose_name='gender')
+    verified = models.BooleanField(default=False, verbose_name='verified')
+    status = models.IntegerField(null=True, choices=StatusType.get_choices(), verbose_name='status')
+    email_verify_date = models.DateTimeField(null=True, verbose_name='email_verify_date')
+
     USERNAME_FIELD = 'id'
+
+    equal_fields = (
+        'idx', 'id', 'name', 'reg_date', 'ip', 'device_id', 'email', 'birth_date', 'gender', 'verified', 'status', 'email_verify_date'
+    )
 
     objects = UserManager()
 
