8d19861589f8c8941f2079b32a34b9a43d9d2bb1
seungmin paik <sm.paik@ridi.com>
2019-03-28 19:35:55 +0900

    user repository 정리

--- src/apps/domains/account/repositories.py
+++ src/apps/domains/account/repositories.py
@@ -26,20 +26,3 @@ class UserRepository(BaseRepository):
     @classmethod
     def find_by_u_idxes(cls, u_idxes: List[int]) -> List[User]:
         return cls.model_class.objects.filter(idx__in=u_idxes)
-
-    @classmethod
-    def update(cls, entities: List, update_fields: List = None):
-        cls._create_user_modified_histories_by_users(entities)
-        super().update(entities)
-
-    @classmethod
-    def create(cls, entities: List, is_bulk: bool = False, bulk_bundle_count: int = 1000) -> List[User]:
-        cls._create_user_modified_histories_by_users(entities)
-        return super().create(entities, is_bulk, bulk_bundle_count)
-
-    @staticmethod
-    def _create_user_modified_histories_by_users(users: List[User]):
-        user_modified_histories = []
-        for user in users:
-            user_modified_histories.append(UserModifiedHistory(u_idx=user.idx))
-        UserModifiedHistoryRepository.create(user_modified_histories, True)
