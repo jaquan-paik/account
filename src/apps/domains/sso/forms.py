fddfc88a1acb2bde5e99936756bd8c35491d7a64
김정근 <snorlax@ridi.com>
2019-04-03 18:39:59 +0900

    form, serializer 추가

--- src/apps/domains/sso/forms.py
+++ src/apps/domains/sso/forms.py
@@ -6,15 +6,17 @@ from lib.utils.url import is_url
 
 
 class SSOLoginForm(forms.Form):
-    token = forms.CharField(required=True, label='SSO 토큰')
-    redirect_uri = forms.CharField(required=True, label='Redirect URI')
+    otp = forms.CharField(required=True, label='SSO Otp')
+    redirect_uri = forms.CharField(label='Redirect URI')
 
     def __init__(self, *args, domain: str, **kwargs):
         super().__init__(*args, **kwargs)
         self.domain = domain
 
-    def clean_next(self):
-        data = self.cleaned_data['redirect_uri']
+    def clean_redirect_uri(self):
+        data = self.cleaned_data.get('redirect_uri', None)
+        if data is None:
+            return data
 
         if not is_url(data):
             raise forms.ValidationError('URI를 입력해주세요.')
--- src/apps/domains/sso/serializers.py
+++ src/apps/domains/sso/serializers.py
@@ -1,24 +1,19 @@
 from rest_framework import serializers
 
-from apps.domains.sso.constants import SSOKeyHint
 from lib.base.serializers import BaseSerializer
 
 
-class BaseSSOSerializer(BaseSerializer):
-    hint = serializers.ChoiceField(choices=SSOKeyHint.get_choices(), required=True, label='SSO Key 힌트')
-
-
-class GenerateTokenRequestSerializer(BaseSSOSerializer):
+class SSOOtpGenerateRequestSerializer(BaseSerializer):
     pass
 
 
-class GenerateTokenResponseSerializer(BaseSerializer):
-    token = serializers.CharField(required=True, label='SSO 토큰')
+class SSOOtpGenerateResponseSerializer(BaseSerializer):
+    otp = serializers.CharField(required=True, label='SSO Otp')
 
 
-class VerifyTokenRequestSerializer(BaseSSOSerializer):
-    token = serializers.CharField(required=True, label='SSO 토큰')
+class SSOOtpVerifyRequestSerializer(BaseSerializer):
+    otp = serializers.CharField(required=True, label='SSO Otp')
 
 
-class VerifyTokenResponseSerializer(BaseSerializer):
-    u_idx = serializers.IntegerField(required=True, label='유저 Idx')
+class SSOOtpVerifyResponseSerializer(BaseSerializer):
+    u_idx = serializers.IntegerField(required=True, label='유저 IDX')
