83e3c241328a1e6750b35e8542109c1bc141252d
김정근 <snorlax@ridi.com>
2019-04-03 18:40:09 +0900

    미사용 상수 제거

--- src/apps/domains/sso/constants.py
+++ src/apps/domains/sso/constants.py
@@ -1,14 +1,1 @@
-from lib.base.constants import BaseConstant
-
 SSO_TOKEN_TTL = 60  # 60초
-
-
-class SSOKeyHint(BaseConstant):
-    VIEWER = 'viewer'
-    SESSION_LOGIN = 'session_login'
-
-    _LIST = [VIEWER, SESSION_LOGIN]
-    _STRING_MAP = {
-        VIEWER: '뷰어',
-        SESSION_LOGIN: '스토어 세션 로그인'
-    }
