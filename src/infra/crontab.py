86dcc467746c02b290471c790a89417bd7585c2b
seungmin paik <sm.paik@ridi.com>
2019-04-04 16:29:09 +0900

    command name 상수화

--- src/infra/crontab.py
+++ src/infra/crontab.py
@@ -1,21 +1,22 @@
 from django.core.management import call_command
 from uwsgidecorators import timer
 
+from infra.configure.constants import CommandName
 from lib.lock.decorators import f_lock
 
 
 @timer(300)
 def revoke_expired_tokens(signum: int):
-    call_command('revoke_expired_tokens')
+    call_command(CommandName.REVOKE_EXPIRED_TOKENS)
 
 
 @timer(1)
-@f_lock('set_user_modified_history_order')
+@f_lock(CommandName.SET_USER_MODIFIED_HISTORY_ORDER)
 def set_user_modified_history_order(signum: int):
-    call_command('set_user_modified_history_order')
+    call_command(CommandName.SET_USER_MODIFIED_HISTORY_ORDER)
 
 
 @timer(1)
-@f_lock('crawl_store_user')
+@f_lock(CommandName.CRAWL_STORE_USER)
 def crawl_store_user(signum: int):
-    call_command('crawl_store_user')
+    call_command(CommandName.CRAWL_STORE_USER)
