7ac0405c0e59e7762b332e979ae7d6714d741e49
seungmin paik <sm.paik@ridi.com>
2019-04-05 14:49:29 +0900

    url에 token view 설정

--- src/apps/domains/oauth2/views.py
+++ src/apps/domains/oauth2/views.py
@@ -1,11 +1,15 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.views import View
 from oauthlib.oauth2 import OAuth2Error
+from rest_framework.views import APIView
 
+from apps.domains.oauth2.exceptions import UnsupportedGrantType
 from apps.domains.oauth2.forms import AuthorizationCodeForm
+from apps.domains.oauth2.serializers import GrantTypeSerializer
 from apps.domains.oauth2.services.oauth2_authorization_code_service import OAuth2AuthorizationCodeService
 from lib.base.response import get_invalid_form_template_response, get_template_response
 from lib.decorators.session_login import ridibooks_session_login_required
+from lib.django.views.api.mixins import ResponseMixin
 from lib.utils.url import generate_query_url
 
 
@@ -28,3 +32,13 @@ class AuthorizationView(View):
 
         redirect_uri = generate_query_url(cleaned_data['redirect_uri'], redirect_param)
         return HttpResponseRedirect(redirect_uri)
+
+
+class TokenView(ResponseMixin, APIView):
+    def post(self, request):
+        grant_type_serializer = GrantTypeSerializer(data=request.data)
+        try:
+            if not grant_type_serializer.is_valid():
+                raise UnsupportedGrantType
+        except OAuth2Error as e:
+            return JsonResponse(data={"error": e.error, "description": e.description}, status=e.status_code)
