7a6b8a65ee2005e7b5a96db6bd4b3f11bee49b91
seungmin paik <sm.paik@ridi.com>
2019-04-09 09:10:09 +0900

    refresh token repository 에서 token과 expire 지정하는 로직 제거

--- src/apps/domains/oauth2/repositories/refresh_token_repository.py
+++ src/apps/domains/oauth2/repositories/refresh_token_repository.py
@@ -1,18 +1,11 @@
-import uuid
-from datetime import timedelta, datetime
-
 from apps.domains.account.models import User
-from apps.domains.oauth2.constants import REFRESH_TOKEN_EXPIRE_DAYS
 from apps.domains.oauth2.models import RefreshToken, Application as Client
 
 
 class RefreshTokenRepository:
     @staticmethod
     def create_refresh_token(client: Client, user: User, scope: str) -> RefreshToken:
-        token = uuid.uuid4().hex
-        expires = datetime.now() + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
-
-        refresh_token = RefreshToken(application=client, user=user, scope=scope, token=token, expires=expires)
+        refresh_token = RefreshToken(application=client, user=user, scope=scope)
         refresh_token.save()
         return refresh_token
 
