3846213899ac82ff493e3f493ebc398ad1ea61a5
seungmin paik <sm.paik@ridi.com>
2019-04-11 10:45:37 +0900

    test service 수정

--- src/apps/domains/ridi/tests/test_service.py
+++ src/apps/domains/ridi/tests/test_service.py
@@ -1,11 +1,10 @@
-import requests_mock
+from datetime import datetime
+
 from django.core.exceptions import PermissionDenied
 from django.test import TestCase
 from django_dynamic_fixture import G
-
 from apps.domains.account.models import User
 from apps.domains.ridi.dtos import TokenData
-from apps.domains.ridi.helpers.url_helper import UrlHelper
 from apps.domains.ridi.services.token_refresh_service import TokenRefreshService
 from apps.domains.oauth2.models import Application, RefreshToken
 
@@ -17,7 +16,7 @@ class TokenRefreshServiceTestCase(TestCase):
         in_house_client = G(Application, skip_authorization=True, user=None, is_in_house=True)
         self.refresh_token = G(
             RefreshToken, application=in_house_client, user=user, scope='all', token='this-is-refresh-token',
-            access_token=None
+            access_token=None, expires=datetime(year=9999, month=12, day=31)
         )
 
         not_in_house_client = G(Application, skip_authorization=True, user=None, is_in_house=False)
@@ -36,19 +35,6 @@ class TokenRefreshServiceTestCase(TestCase):
             TokenRefreshService.get_tokens(self.not_in_house_refresh_token.token)
 
     def test_success_refresh(self):
-        with requests_mock.mock() as m:
-            m.post(UrlHelper.get_oauth2_token_url(), json={
-                'access_token': 'test-access-token2222',
-                'expires_in': 1111111,
-                'refresh_token': 'test-refresh-token2222',
-                'refresh_token_expires_in': 2222222,
-            })
-
-            at, rt = TokenRefreshService.get_tokens(self.refresh_token.token)
-            self.assertIsInstance(at, TokenData)
-            self.assertIsInstance(rt, TokenData)
-
-            self.assertEqual(at.token, 'test-access-token2222')
-            self.assertEqual(at.expires_in, 1111111)
-            self.assertEqual(rt.token, 'test-refresh-token2222')
-            self.assertEqual(rt.expires_in, 2222222)
+        at, rt = TokenRefreshService.get_tokens(self.refresh_token.token)
+        self.assertIsInstance(at, TokenData)
+        self.assertIsInstance(rt, TokenData)
